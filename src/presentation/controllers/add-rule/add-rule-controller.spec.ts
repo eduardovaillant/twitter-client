b60a996bfba3006d24413b7d58eeb4bc3a4c7246
eduardovaillant <eduardo.bvaillant@gmail.com>
2021-02-11 19:04:40 -0300

    refactor: move sut creation to a factory method

--- src/presentation/controllers/add-rule/add-rule-controller.spec.ts
+++ src/presentation/controllers/add-rule/add-rule-controller.spec.ts
@@ -1,23 +1,43 @@
 import { AddRuleController } from './add-rule-controller'
 import { AddRule, AddRuleModel } from '@/domain/usecases/add-rule'
 import { RuleModel } from '@/domain/models/rule-model'
+import { HttpRequest } from '@/presentation/protocols/http'
+
+const makeAddRuleStub = (): AddRule => {
+  class AddRulesStub implements AddRule {
+    async add (rule: AddRuleModel): Promise<RuleModel> {
+      return await Promise.resolve({ id: 'any_id', value: 'any_value', tag: 'any_tag' })
+    }
+  }
+  return new AddRulesStub()
+}
+
+const makeFakeRule = (): AddRuleModel => ({
+  value: 'any_value'
+})
+
+const makeFakeRequest = (): HttpRequest => ({
+  body: makeFakeRule()
+})
+
+type SutTypes = {
+  sut: AddRuleController
+  addRuleStub: AddRule
+}
+const makeSut = (): SutTypes => {
+  const addRuleStub = makeAddRuleStub()
+  const sut = new AddRuleController(addRuleStub)
+  return {
+    sut,
+    addRuleStub
+  }
+}
 
 describe('AddRuleController', () => {
   test('should call add rule with correct values', async () => {
-    class AddRulesStub implements AddRule {
-      async add (rule: AddRuleModel): Promise<RuleModel> {
-        return await Promise.resolve({ id: 'any_id', value: 'any_value', tag: 'any_tag' })
-      }
-    }
-    const addRulesStub = new AddRulesStub()
-    const sut = new AddRuleController(addRulesStub)
-    const addSpy = jest.spyOn(addRulesStub, 'add')
-    const fakeRule = {
-      value: 'any_value'
-    }
-    await sut.handle({
-      body: fakeRule
-    })
-    expect(addSpy).toBeCalledWith(fakeRule)
+    const { sut, addRuleStub } = makeSut()
+    const addSpy = jest.spyOn(addRuleStub, 'add')
+    await sut.handle(makeFakeRequest())
+    expect(addSpy).toBeCalledWith(makeFakeRule())
   })
 })
