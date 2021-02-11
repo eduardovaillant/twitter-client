6930bb192f1e7906062258cb381ff5854a191e49
eduardovaillant <eduardo.bvaillant@gmail.com>
2021-02-11 19:18:52 -0300

    feat: ensure AddRuleController returns 400 if no value is provided

--- src/presentation/controllers/add-rule/add-rule-controller.ts
+++ src/presentation/controllers/add-rule/add-rule-controller.ts
@@ -1,6 +1,8 @@
 import { Controller } from '@/presentation/protocols/controller'
 import { HttpRequest, HttpResponse } from '@/presentation/protocols/http'
 import { AddRule } from '@/domain/usecases/add-rule'
+import { badRequest } from '@/presentation/helpers/http-helper'
+import { MissingParamError } from '@/presentation/errors/missing-param-error'
 
 export class AddRuleController implements Controller {
   constructor (
@@ -8,6 +10,12 @@ export class AddRuleController implements Controller {
   ) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const { value } = httpRequest.body
+
+    if (!value) {
+      return badRequest(new MissingParamError('value'))
+    }
+
     await this.addRule.add(httpRequest.body)
     return Promise.resolve(null)
   }
---
+++ src/presentation/helpers/http-helper.ts
@@ -0,0 +1,11 @@
+import { HttpResponse } from '@/presentation/protocols/http'
+
+export const badRequest = (error: Error): HttpResponse => (
+  {
+    statusCode: 400,
+    body: {
+      code: 400,
+      error
+    }
+  }
+)
