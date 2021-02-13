342ae167a5ddc53b280daeaa46a52853e3c07fe9
eduardovaillant <eduardo.bvaillant@gmail.com>
2021-02-13 14:12:23 -0300

    test: ensure AddRuleValidator returns an error if tag max lenght validation fails

--- src/validation/validators/add-rule-validator.spec.ts
+++ src/validation/validators/add-rule-validator.spec.ts
@@ -28,4 +28,11 @@ describe('AddRuleValidator', () => {
     expect(result.isValid).toBeFalsy()
     expect(result.errors[0]).toBe('"tag" length must be at least 3 characters long')
   })
+
+  test('should return an error if value length is more than the max length required', () => {
+    const sut = new AddRuleValidator()
+    const result = sut.validate({ value: 'any_value', tag: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa' })
+    expect(result.isValid).toBeFalsy()
+    expect(result.errors[0]).toBe('"tag" length must be less than or equal to 30 characters long')
+  })
 })
