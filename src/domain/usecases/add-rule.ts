edd1f5a786ff6f9cdd7b1c66fe8d384a692e815f
eduardovaillant <eduardo.bvaillant@gmail.com>
2021-02-13 15:51:01 -0300

    refactor: change twitter_rule_id to required

--- src/domain/usecases/add-rule.ts
+++ src/domain/usecases/add-rule.ts
@@ -1,6 +1,6 @@
 import { RuleModel } from '@/domain/models/rule-model'
 
-export type AddRuleModel = Omit<RuleModel, 'id'>
+export type AddRuleModel = Omit<RuleModel, 'id'| 'twitter_rule_id'>
 
 export interface AddRule {
   add: (rule: AddRuleModel) => Promise<RuleModel>
