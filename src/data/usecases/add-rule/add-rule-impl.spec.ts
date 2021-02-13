f74d6c6fbb84346804735bdefb0b4fcc11dbbaa7
eduardovaillant <eduardo.bvaillant@gmail.com>
2021-02-13 16:26:11 -0300

    feat: test AddRuleImpl calls AddRuleRepository with correct values

--- src/data/usecases/add-rule/add-rule-impl.spec.ts
+++ src/data/usecases/add-rule/add-rule-impl.spec.ts
@@ -1,6 +1,8 @@
 import { AddRuleImpl } from './add-rule-impl'
+import { AddRuleRepository } from '@/data/protocols/repositories/add-rule-repository'
 import { TwitterAddRule, TwitterAddRuleResponse } from '@/data/protocols/clients/twitter-add-rule'
 import { AddRuleModel } from '@/domain/usecases/add-rule'
+import { RuleModel } from '@/domain/models/rule-model'
 
 const makeTwitterAddRuleStub = (): TwitterAddRule => {
   class TwitterAddRuleStub implements TwitterAddRule {
@@ -15,6 +17,20 @@ const makeTwitterAddRuleStub = (): TwitterAddRule => {
   return new TwitterAddRuleStub()
 }
 
+const makeAddRuleRepositoryStub = (): AddRuleRepository => {
+  class AddRuleRepositoryStub implements AddRuleRepository {
+    async addRule (rule: TwitterAddRuleResponse): Promise<RuleModel> {
+      return {
+        id: 'any_id',
+        value: 'any_value',
+        tag: 'any_tag',
+        twitter_rule_id: 'any_id'
+      }
+    }
+  }
+  return new AddRuleRepositoryStub()
+}
+
 const makeFakeAddRule = (): AddRuleModel => (
   {
     value: 'any_value',
@@ -22,17 +38,28 @@ const makeFakeAddRule = (): AddRuleModel => (
   }
 )
 
+const makeFakeTwitterAddRuleResponse = (): TwitterAddRuleResponse => (
+  {
+    value: 'any_value',
+    tag: 'any_tag',
+    twitter_rule_id: 'any_id'
+  }
+)
+
 type SutTypes = {
   sut: AddRuleImpl
   twitterAddRuleStub: TwitterAddRule
+  addRuleRepositoryStub: AddRuleRepository
 }
 
 const makeSut = (): SutTypes => {
   const twitterAddRuleStub = makeTwitterAddRuleStub()
-  const sut = new AddRuleImpl(twitterAddRuleStub)
+  const addRuleRepositoryStub = makeAddRuleRepositoryStub()
+  const sut = new AddRuleImpl(twitterAddRuleStub, addRuleRepositoryStub)
   return {
     sut,
-    twitterAddRuleStub
+    twitterAddRuleStub,
+    addRuleRepositoryStub
   }
 }
 
@@ -49,5 +76,12 @@ describe('AddRuleImpl', () => {
     jest.spyOn(twitterAddRuleStub, 'addRule').mockImplementationOnce(() => { throw new Error() })
     const promise = sut.add(makeFakeAddRule())
     await expect(promise).rejects.toThrow()
+  })
+
+  test('should call the AddRuleRepository with correct values', async () => {
+    const { sut, addRuleRepositoryStub } = makeSut()
+    const addRuleSpy = jest.spyOn(addRuleRepositoryStub, 'addRule')
+    await sut.add(makeFakeAddRule())
+    expect(addRuleSpy).toHaveBeenCalledWith(makeFakeTwitterAddRuleResponse())
   })
 })
