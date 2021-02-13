2c9ceeb31d27dd46f954eb45e176518c9c88b4fe
eduardovaillant <eduardo.bvaillant@gmail.com>
2021-02-13 14:04:28 -0300

    test: ensure AddRuleValidator returns an error if min length validation fails

--- src/validation/validators/add-rule-validator.spec.ts
+++ src/validation/validators/add-rule-validator.spec.ts
@@ -7,4 +7,11 @@ describe('AddRuleValidator', () => {
     expect(result.isValid).toBeFalsy()
     expect(result.errors[0]).toBe('"value" is required')
   })
+
+  test('should return an error if value does not have the min length required', () => {
+    const sut = new AddRuleValidator()
+    const result = sut.validate({ value: 'ad' })
+    expect(result.isValid).toBeFalsy()
+    expect(result.errors[0]).toBe('"value" length must be at least 3 characters long')
+  })
 })
