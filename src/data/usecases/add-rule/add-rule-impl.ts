9d1147c5916ce5164cb1f1b1d504eff3d8ee725e
eduardovaillant <eduardo.bvaillant@gmail.com>
2021-02-13 16:25:47 -0300

    feat: ensure AddRuleImpl calls AddRuleRepository with correct values

--- src/data/usecases/add-rule/add-rule-impl.ts
+++ src/data/usecases/add-rule/add-rule-impl.ts
@@ -1,14 +1,17 @@
 import { TwitterAddRule } from '@/data/protocols/clients/twitter-add-rule'
+import { AddRuleRepository } from '@/data/protocols/repositories/add-rule-repository'
 import { RuleModel } from '@/domain/models/rule-model'
 import { AddRule, AddRuleModel } from '@/domain/usecases/add-rule'
 
 export class AddRuleImpl implements AddRule {
   constructor (
-    private readonly twitterClient: TwitterAddRule
+    private readonly twitterClient: TwitterAddRule,
+    private readonly addRuleRepository: AddRuleRepository
   ) {}
 
   async add (rule: AddRuleModel): Promise<RuleModel> {
-    await this.twitterClient.addRule(rule)
+    const twitterResponse = await this.twitterClient.addRule(rule)
+    await this.addRuleRepository.addRule(twitterResponse)
     return Promise.resolve(null)
   }
 }
