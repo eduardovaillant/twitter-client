4bc4441c16910bf6cf69170a35154ed48cbb6d12
eduardovaillant <eduardo.bvaillant@gmail.com>
2021-02-13 16:08:25 -0300

    refactor: move sut creation to a factory method

--- src/data/usecases/add-rule/add-rule-impl.spec.ts
+++ src/data/usecases/add-rule/add-rule-impl.spec.ts
@@ -2,27 +2,45 @@ import { AddRuleImpl } from './add-rule-impl'
 import { TwitterAddRule, TwitterAddRuleResponse } from '@/data/protocols/clients/twitter-add-rule'
 import { AddRuleModel } from '@/domain/usecases/add-rule'
 
-describe('AddRuleImpl', () => {
-  test('should call the twitter api with correct data', async () => {
-    class TwitterClientStub implements TwitterAddRule {
-      async addRule (rule: AddRuleModel): Promise<TwitterAddRuleResponse> {
-        return {
-          value: 'any_value',
-          tag: 'any_tag',
-          twitter_rule_id: 'any_id'
-        }
+const makeTwitterAddRuleStub = (): TwitterAddRule => {
+  class TwitterAddRuleStub implements TwitterAddRule {
+    async addRule (rule: AddRuleModel): Promise<TwitterAddRuleResponse> {
+      return {
+        value: 'any_value',
+        tag: 'any_tag',
+        twitter_rule_id: 'any_id'
       }
     }
-    const twitterClientStub = new TwitterClientStub()
-    const sut = new AddRuleImpl(twitterClientStub)
-    const addRuleSpy = jest.spyOn(twitterClientStub, 'addRule')
-    await sut.add({
-      value: 'any_value',
-      tag: 'any_tag'
-    })
-    expect(addRuleSpy).toHaveBeenCalledWith({
-      value: 'any_value',
-      tag: 'any_tag'
-    })
+  }
+  return new TwitterAddRuleStub()
+}
+
+const makeFakeAddRule = (): AddRuleModel => (
+  {
+    value: 'any_value',
+    tag: 'any_tag'
+  }
+)
+
+type SutTypes = {
+  sut: AddRuleImpl
+  twitterAddRuleStub: TwitterAddRule
+}
+
+const makeSut = (): SutTypes => {
+  const twitterAddRuleStub = makeTwitterAddRuleStub()
+  const sut = new AddRuleImpl(twitterAddRuleStub)
+  return {
+    sut,
+    twitterAddRuleStub
+  }
+}
+
+describe('AddRuleImpl', () => {
+  test('should call the TwitterAddRule with correct values', async () => {
+    const { sut, twitterAddRuleStub } = makeSut()
+    const addRuleSpy = jest.spyOn(twitterAddRuleStub, 'addRule')
+    await sut.add(makeFakeAddRule())
+    expect(addRuleSpy).toHaveBeenCalledWith(makeFakeAddRule())
   })
 })
