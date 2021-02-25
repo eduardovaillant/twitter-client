6a1cbb278d0f92e3502c680379fbbe251e8b3823
eduardovaillant <eduardo.bvaillant@gmail.com>
2021-02-25 19:19:50 -0300

    refactor: move sut creation to a factory method

--- src/infra/clients/twitter-client.spec.ts
+++ src/infra/clients/twitter-client.spec.ts
@@ -3,11 +3,13 @@ import { AddRuleModel } from '@/domain/usecases/add-rule'
 import { HttpPost } from '../protocols/http-client/http-post'
 import { TwitterClient } from './twitter-client'
 
-const makeFakeTwitterAddRuleResponse = (): TwitterAddRuleResponse => ({
-  value: 'any_value',
-  tag: 'any_tag',
-  twitter_rule_id: 'any_id'
-})
+const makeFakeTwitterAddRuleResponse = (): TwitterAddRuleResponse => (
+  {
+    value: 'any_value',
+    tag: 'any_tag',
+    twitter_rule_id: 'any_id'
+  }
+)
 
 const makeFakeAddRule = (): AddRuleModel => (
   {
@@ -16,17 +18,34 @@ const makeFakeAddRule = (): AddRuleModel => (
   }
 )
 
+const makeHttpPostStub = (): HttpPost => {
+  class HttpPostStub implements HttpPost {
+    async post (data: any, url: string): Promise<any> {
+      return Promise.resolve(makeFakeTwitterAddRuleResponse())
+    }
+  }
+  return new HttpPostStub()
+}
+
+interface SutTypes {
+  sut: TwitterClient
+  httpPostStub: HttpPost
+}
+
+const makeSut = (): SutTypes => {
+  const httpPostStub = makeHttpPostStub()
+  const sut = new TwitterClient(httpPostStub)
+  return {
+    sut,
+    httpPostStub
+  }
+}
+
 describe('TwitterClient', () => {
   describe('Add Rule', () => {
     test('should call the twitter api with correct values ', async () => {
-      class HttpPostStub implements HttpPost {
-        async post (data: any, url: string): Promise<any> {
-          return Promise.resolve(makeFakeTwitterAddRuleResponse())
-        }
-      }
-      const httpPostStub = new HttpPostStub()
+      const { sut, httpPostStub } = makeSut()
       const postSpy = jest.spyOn(httpPostStub, 'post')
-      const sut = new TwitterClient(httpPostStub)
       await sut.addRule(makeFakeAddRule())
       expect(postSpy.mock.calls[0][1]).toEqual(makeFakeAddRule())
     })
