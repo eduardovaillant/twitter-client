6128bd37964def24cb5db6f4c95c635983c42e5e
eduardovaillant <eduardo.bvaillant@gmail.com>
2021-02-25 18:47:48 -0300

    chore: add env

---
+++ src/main/config/env.ts
@@ -0,0 +1,8 @@
+export default {
+  baseUrl: process.env.BASE_URL || '',
+  apiKey: process.env.API_KEY || '',
+  apiSecret: process.env.API_SECRET || '',
+  bearerToken: process.env.BEARER_TOKEN || '',
+  accessToken: process.env.ACCESS_TOKEN || '',
+  accessTokenSecret: process.env.ACCESS_TOKEN_SECRET || ''
+}
