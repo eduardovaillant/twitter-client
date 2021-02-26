a48853d47419481e496aa6a0c4616d5da15837ef
eduardovaillant <eduardo.bvaillant@gmail.com>
2021-02-26 16:33:03 -0300

    feat: esure TwitterClient returns the created rule on success

--- src/infra/clients/twitter-client.ts
+++ src/infra/clients/twitter-client.ts
@@ -2,13 +2,14 @@ import { HttpPost } from '@/infra/protocols/http-client/http-post'
 import { TwitterAddRule, TwitterAddRuleResponse } from '@/data/protocols/clients/twitter-add-rule'
 import { AddRuleModel } from '@/domain/usecases/add-rule'
 
+// TODO if the twitter response is equal null, what i should do?
 export class TwitterClient implements TwitterAddRule {
   constructor (
     private readonly httpPost: HttpPost
   ) {}
 
   async addRule (rule: AddRuleModel): Promise<TwitterAddRuleResponse> {
-    await this.httpPost.post('add_rule_url', rule)
-    return null
+    const result = await this.httpPost.post('add_rule_url', rule)
+    return result
   }
 }
