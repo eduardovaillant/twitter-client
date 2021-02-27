8c5840898c57c44e9fbdf8a8af231e3140b4df7d
eduardovaillant <eduardo.bvaillant@gmail.com>
2021-02-26 21:26:01 -0300

    refactor: change the url on TwitterClient

--- src/infra/clients/twitter-client.ts
+++ src/infra/clients/twitter-client.ts
@@ -1,6 +1,7 @@
 import { HttpPost } from '@/infra/protocols/http-client/http-post'
 import { TwitterAddRule, TwitterAddRuleResponse } from '@/data/protocols/clients/twitter-add-rule'
 import { AddRuleModel } from '@/domain/usecases/add-rule'
+import env from '@/main/config/env'
 
 // TODO if the twitter response is equal null, what i should do?
 export class TwitterClient implements TwitterAddRule {
@@ -9,7 +10,7 @@ export class TwitterClient implements TwitterAddRule {
   ) {}
 
   async addRule (rule: AddRuleModel): Promise<TwitterAddRuleResponse> {
-    const result = await this.httpPost.post('tweets/search/stream/rules', rule)
+    const result = await this.httpPost.post(env.baseUrl + 'tweets/search/stream/rules', rule)
     return result
   }
 }
--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 export default {
   baseUrl: process.env.BASE_URL || '',
   apiKey: process.env.API_KEY || '',
