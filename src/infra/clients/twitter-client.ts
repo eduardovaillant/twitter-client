d2bbb57e5d551d78449855edd176fd7fdfeb1adf
eduardovaillant <eduardo.bvaillant@gmail.com>
2021-02-27 16:38:25 -0300

    feat: ensure TwitterClient calls AddRule with correct config

--- src/infra/clients/twitter-client.ts
+++ src/infra/clients/twitter-client.ts
@@ -3,15 +3,16 @@ import { TwitterAddRule, TwitterAddRuleResponse } from '@/data/protocols/clients
 import { AddRuleModel } from '@/domain/usecases/add-rule'
 import env from '@/main/config/env'
 
-// TODO if the twitter response is equal null, what i should do?
-// TODO add tests: url and authorization
 export class TwitterClient implements TwitterAddRule {
   constructor (
     private readonly httpPost: HttpPost
   ) {}
 
   async addRule (rule: AddRuleModel): Promise<TwitterAddRuleResponse> {
-    const result = await this.httpPost.post(env.baseUrl + 'tweets/search/stream/rules', rule)
+    const config = {
+      headers: { Authorization: `Bearer ${env.bearerToken}` }
+    }
+    const result = await this.httpPost.post(env.baseUrl + 'tweets/search/stream/rules', rule, config)
     return result
   }
 }
