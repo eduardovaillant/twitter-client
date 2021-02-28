a612f73cb3e32389db18961f26ef96d95a3b3f59
eduardovaillant <eduardo.bvaillant@gmail.com>
2021-02-28 17:20:26 -0300

    feat: ensure TweetRepository creates a rule on success

---
+++ src/infra/db/tweet/tweet-repository.ts
@@ -0,0 +1,10 @@
+import { AddRuleRepository, RuleModel, TwitterAddRuleResponse } from '@/data/usecases/add-rule/add-rule-impl-protocols'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class TweetRepository implements AddRuleRepository {
+  async addRule (rule: TwitterAddRuleResponse): Promise<RuleModel> {
+    const ruleCollection = await MongoHelper.getCollection('rules')
+    const result = await ruleCollection.insertOne(rule)
+    return MongoHelper.map(result.ops[0])
+  }
+}
