7e50edee8a5da2a0d0b2e4bef3f288be3dbc7f7e
eduardovaillant <eduardo.bvaillant@gmail.com>
2021-03-02 20:34:45 -0300

    chore: add mongoUrl

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,7 +2,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/twitter-client',
   port: process.env.PORT || 3000,
   baseUrl: process.env.BASE_URL || '',
   apiKey: process.env.API_KEY || '',
