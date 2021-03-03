799797017c0d6e757d5adc12ce07ed1db64a2a55
eduardovaillant <eduardo.bvaillant@gmail.com>
2021-03-03 17:58:24 -0300

    feat: ensure TweetRepository saves the tweet on success

--- src/infra/db/tweet/tweet-repository.ts
+++ src/infra/db/tweet/tweet-repository.ts
@@ -1,10 +1,17 @@
+import { MongoHelper } from '@/infra/db/helpers/mongo-helper'
+import { SaveTweetRepository } from '@/data/protocols/repositories/save-tweet-repository'
 import { AddRuleRepository, RuleModel, TwitterAddRuleResponse } from '@/data/usecases/rules/add-rule/add-rule-impl-protocols'
-import { MongoHelper } from '../helpers/mongo-helper'
+import { SaveTweetModel } from '@/domain/usecases/save-tweet'
 
-export class TweetRepository implements AddRuleRepository {
+export class TweetRepository implements AddRuleRepository, SaveTweetRepository {
   async addRule (rule: TwitterAddRuleResponse): Promise<RuleModel> {
-    const ruleCollection = await MongoHelper.getCollection('rules')
-    const result = await ruleCollection.insertOne(rule)
+    const rulesCollection = await MongoHelper.getCollection('rules')
+    const result = await rulesCollection.insertOne(rule)
     return MongoHelper.map(result.ops[0])
+  }
+
+  async saveTweet (tweet: SaveTweetModel): Promise<void> {
+    const tweetsCollection = await MongoHelper.getCollection('tweets')
+    await tweetsCollection.insertOne(tweet)
   }
 }
