b5ebd938911d61fe29714e6c32c888f9a3380346
eduardovaillant <eduardo.bvaillant@gmail.com>
2021-03-03 17:58:47 -0300

    test: ensure TweetRepository saves the tweet on success

--- src/infra/db/tweet/tweet-repository.spec.ts
+++ src/infra/db/tweet/tweet-repository.spec.ts
@@ -4,6 +4,7 @@ import { TwitterAddRuleResponse } from '@/data/protocols/clients/twitter-add-rul
 import { Collection } from 'mongodb'
 
 let accountCollection: Collection
+let tweetCollection: Collection
 
 const makeFakeTwitterAddRuleResponse = (): TwitterAddRuleResponse => (
   {
@@ -24,7 +25,9 @@ describe('TweetRepository', () => {
 
   beforeEach(async () => {
     accountCollection = await MongoHelper.getCollection('rules')
+    tweetCollection = await MongoHelper.getCollection('tweets')
     await accountCollection.deleteMany({})
+    await tweetCollection.deleteMany({})
   })
 
   const makeSut = (): TweetRepository => {
@@ -32,7 +35,7 @@ describe('TweetRepository', () => {
   }
 
   describe('addRule()', () => {
-    test('Should return an account on add success', async () => {
+    test('Should return a rule on addRule success', async () => {
       const sut = makeSut()
       const rule = await sut.addRule(makeFakeTwitterAddRuleResponse())
       expect(rule).toBeTruthy()
@@ -40,6 +43,20 @@ describe('TweetRepository', () => {
       expect(rule.tag).toBe('any_tag')
       expect(rule.value).toBe('any_value')
       expect(rule.twitter_rule_id).toBe('any_id')
+    })
+  })
+
+  describe('saveTweet()', () => {
+    test('Should save the tweet on saveTweet success', async () => {
+      const sut = makeSut()
+      await sut.saveTweet({
+        text: 'any_text',
+        tweet_id: 'any_id'
+      })
+      const tweet = await tweetCollection.findOne({ tweet_id: 'any_id' })
+      expect(tweet).toBeTruthy()
+      expect(tweet._id).toBeTruthy()
+      expect(tweet.tweet_id).toBe('any_id')
     })
   })
 })
